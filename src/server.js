6ca20ae6938744e697c94b2b21cb79da26368ed2
DESKTOP-1F7SLP5\andye <andy.ellingson@outlook.com>
2020-05-10 10:20:46 -0500

    changed server js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,19 @@
-//Install express server    
+//Install express server
 const express = require('express');
+const path = require('path');
 
-const path = require('path');   
+const app = express();
 
-const app = express();   
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-// Serve only the static files form the dist directory    
-app.use(express.static(__dirname));
+app.get('/*', function(req,res) {
 
-app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname + 'index.html'));   
-});  
+res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
 
-// Start the app by listening on the default Heroku port    
-app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Express server listening on port', port)
+});
